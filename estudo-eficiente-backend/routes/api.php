722d5cb7feaf714a89de0008357168dc5aee8a99
Adriano-Linux <adriano_silva131@outlook.com>
2025-07-23 18:42:09 -0300

    removendo imports desnecessários

--- estudo-eficiente-backend/routes/api.php
+++ estudo-eficiente-backend/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MateriaController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
