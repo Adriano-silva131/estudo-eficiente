774dd0e92cc641f589f585172452ca0f36c9b185
Adriano <adriano_silva131@outlook.com>
2025-08-25 13:02:48 -0300

    alterando alguns designs e criando os compontes de materias e pomodoro

--- estudo-eficiente-frontend/src/modules/Materias/MateriasRoutes.jsx
+++ estudo-eficiente-frontend/src/modules/Materias/MateriasRoutes.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Materia from "./Materias";
+import TipoMaterias from "./TipoMaterias";
 
 const MateriasRoutes = [
   <Route path="materia" element={<Materia />} key="materia" />,
+  <Route path="tipomateria" element={<TipoMaterias />} key="tipomateria" />,
 ];
 
 export default MateriasRoutes;
